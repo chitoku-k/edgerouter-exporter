acf979143a41197481c26f2200a344fc0ad6ad41
Chitoku <chitoku-k@users.noreply.github.com>
2021-11-23 00:52:01 +0900

    Update running commands to not execute sudo (#49)

--- infrastructure/config/env.go
+++ infrastructure/config/env.go
@@ -7,10 +7,12 @@ import (
 )
 
 type Environment struct {
-	Port      string
-	TLSCert   string
-	TLSKey    string
-	OpCommand string
+	Port          string
+	TLSCert       string
+	TLSKey        string
+	OpCommand     string
+	OpDdnsCommand string
+	VtyshCommand  string
 }
 
 func Get() (Environment, error) {
@@ -18,9 +20,11 @@ func Get() (Environment, error) {
 	var env Environment
 
 	for k, v := range map[string]*string{
-		"TLS_CERT":   &env.TLSCert,
-		"TLS_KEY":    &env.TLSKey,
-		"OP_COMMAND": &env.OpCommand,
+		"TLS_CERT":        &env.TLSCert,
+		"TLS_KEY":         &env.TLSKey,
+		"OP_COMMAND":      &env.OpCommand,
+		"OP_DDNS_COMMAND": &env.OpDdnsCommand,
+		"VTYSH_COMMAND":   &env.VtyshCommand,
 	} {
 		*v = os.Getenv(k)
 	}
@@ -41,6 +45,14 @@ func Get() (Environment, error) {
 
 	if env.OpCommand == "" {
 		env.OpCommand = "/opt/vyatta/bin/vyatta-op-cmd-wrapper"
+	}
+
+	if env.OpDdnsCommand == "" {
+		env.OpDdnsCommand = "/opt/vyatta/bin/sudo-users/vyatta-op-dynamic-dns.pl"
+	}
+
+	if env.VtyshCommand == "" {
+		env.VtyshCommand = "/opt/vyatta/sbin/ubnt_vtysh"
 	}
 
 	return env, nil
