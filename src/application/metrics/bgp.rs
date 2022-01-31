65ec3488de52b5911b0668ce7b4faf91a9aa7cb0
Chitoku <odango@chitoku.jp>
2022-02-01 01:34:36 +0900

    Directly depend on atomic-shim

--- src/application/metrics/bgp.rs
+++ src/application/metrics/bgp.rs
@@ -1,11 +1,11 @@
 use prometheus_client::{
     encoding::text::Encode,
-    metrics::{family::Family, gauge::Gauge},
+    metrics::family::Family,
     registry::Registry,
 };
 
 use crate::{
-    application::metrics::Collector,
+    application::metrics::{Collector, Gauge},
     domain::bgp::{BGPIterator, BGPNeighbor},
     service::bgp::BGPStatusResult,
 };
