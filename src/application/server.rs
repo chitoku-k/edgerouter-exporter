51797baf99436f72dafd1c63ffa6e64346a47415
Chitoku <chitoku-k@users.noreply.github.com>
2022-04-10 00:00:20 +0900

    Reload TLS certs automatically when rotated (#91)

--- src/application/server.rs
+++ src/application/server.rs
@@ -1,4 +1,9 @@
 use std::{net::Ipv6Addr, sync::Arc};
+#[cfg(feature = "tls")]
+use std::{
+    sync::mpsc::channel,
+    time::Duration,
+};
 
 use async_trait::async_trait;
 use axum::{
@@ -6,7 +11,12 @@ use axum::{
     routing::get,
     Router,
 };
+#[cfg(feature = "tls")]
 use axum_server::tls_rustls::RustlsConfig;
+#[cfg(feature = "tls")]
+use notify::{DebouncedEvent, Watcher};
+#[cfg(feature = "tls")]
+use tokio::task::JoinHandle;
 
 use crate::application::metrics;
 
@@ -48,19 +58,22 @@ where
             .route("/", get(metrics::handle::<MetricsController>))
             .layer(Extension(Arc::new(self.metrics_controller)));
 
+        let addr = (Ipv6Addr::UNSPECIFIED, self.port).into();
         let app = Router::new()
             .nest("/healthz", health)
             .nest("/metrics", metrics);
-        let addr = (Ipv6Addr::UNSPECIFIED, self.port).into();
 
-        match &self.tls {
+        match self.tls {
             #[cfg(not(feature = "tls"))]
             Some(_) => {
                 panic!("TLS is not enabled.");
             },
             #[cfg(feature = "tls")]
             Some((tls_cert, tls_key)) => {
-                axum_server::bind_rustls(addr, RustlsConfig::from_pem_file(tls_cert, tls_key).await?)
+                let config = RustlsConfig::from_pem_file(&tls_cert, &tls_key).await?;
+                enable_auto_reload(config.clone(), tls_cert, tls_key);
+
+                axum_server::bind_rustls(addr, config)
                     .serve(app.into_make_service())
                     .await?;
             },
@@ -73,4 +86,21 @@ where
 
         Ok(())
     }
+}
+
+#[cfg(feature = "tls")]
+fn enable_auto_reload(config: RustlsConfig, tls_cert: String, tls_key: String) -> JoinHandle<anyhow::Result<()>> {
+    let (tx, rx) = channel();
+
+    tokio::spawn(async move {
+        let mut watcher = notify::watcher(tx, Duration::from_secs(1))?;
+        watcher.watch(&tls_cert, notify::RecursiveMode::NonRecursive)?;
+
+        loop {
+            let event = rx.recv()?;
+            if let DebouncedEvent::Write(_) = event {
+                config.reload_from_pem_file(&tls_cert, &tls_key).await?;
+            }
+        }
+    })
 }
